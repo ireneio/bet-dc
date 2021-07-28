4a71d34dea4d80a2ffceb701e051b8e8f08ee685
Your Name <you@example.com>
2021-07-29 03:02:36 +0800

    clean-up

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 import express, { Application, Request, Response } from "express"
-import * as path from 'path'
-import cookieParser from 'cookie-parser'
+// import * as path from 'path'
+// import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-import cors from 'cors'
+// import cors from 'cors'
 
 import indexRouter from './routes/index'
 
@@ -14,10 +14,10 @@ runPuppeteer()
 const app: Application = express()
 
 // cors
-const corsOptions = {
-  origin: process.env.NODE_APP_CORS_URL || 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// const corsOptions = {
+//   origin: process.env.NODE_APP_CORS_URL || 'http://localhost:3000',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 // app.use(cors(corsOptions))
 
