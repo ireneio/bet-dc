2bb3610d2ef11ca18155d4583f3a8162e760f5f5
Irene <keaiko15678@gmail.com>
2021-08-02 20:22:22 +0800

    update-deploy

--- docker-local.sh
+++ docker-local.sh
@@ -3,5 +3,10 @@
 # docker build --file $2 -t $1 .
 # docker run -it -p 8081:8081 $1
 
-docker build -f Dockerfile -t snkr-crawler .
+docker build -f Dockerfile.prd -t snkr-crawler .
+
+
+docker buildx build --platform linux/amd64,linux/arm64 --push -t registry.digitalocean.com/snkr/snkr-crawler -f Dockerfile.prd .
+
+
 docker run -it -p 8081:8081 snkr-crawler
