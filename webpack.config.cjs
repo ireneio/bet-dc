7a3563c90a1b0afcd9e4cd0f79e08abb05f9f53d
Irene <keaiko15678@gmail.com>
2021-08-13 15:11:19 +0800

    update-env

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,52 +2,55 @@ const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 const Dotenv = require('dotenv-webpack')
 
-const config = {
-  mode: 'development',
-  target: 'node',
-  externals: [nodeExternals()],
-  externalsType: "import",
-  devtool: 'inline-source-map',
-  entry: {
-    'app': './bin/www.ts'
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(mjs|js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        loader: "ts-loader"
-      }
-    ],
-  },
-  experiments: {
-    topLevelAwait: true
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    fallback: {
-      http: require.resolve("http")
+const config = (env) => {
+  const { config } = env
+  return {
+    mode: 'production',
+    target: 'node',
+    externals: [nodeExternals()],
+    externalsType: "import",
+    devtool: 'inline-source-map',
+    entry: {
+      'app': './bin/www.ts'
     },
-    alias: {
-      '~': path.resolve(__dirname, '')
-    }
-  },
-  plugins: [
-    new Dotenv({
-      path: './.env',
-      safe: true,
-      allowEmptyValues: true
-    })
-  ]
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(mjs|js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          loader: "ts-loader"
+        }
+      ],
+    },
+    experiments: {
+      topLevelAwait: true
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      fallback: {
+        http: require.resolve("http")
+      },
+      alias: {
+        '~': path.resolve(__dirname, '')
+      }
+    },
+    plugins: [
+      new Dotenv({
+        path: `./.env.${config}`,
+        safe: true,
+        allowEmptyValues: true
+      })
+    ]
+  }
 }
 
 module.exports = config
